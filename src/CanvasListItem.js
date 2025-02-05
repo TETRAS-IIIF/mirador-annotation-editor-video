9103fdafcc7c9a86e545f8e5d02a232940ee631a
Drilio <Roy-Antoine93@proton.me>
2025-02-05 09:53:53 +0100

    linter

--- src/CanvasListItem.js
+++ src/CanvasListItem.js
@@ -84,9 +84,7 @@ const CanvasListItem = forwardRef((props, ref) => {
         return flatten(Object.entries(annotationsOnCanvases[canvas.id])
           .map(([key, value]) => {
             if (value.json && value.json.items) {
-              return value.json.items.filter((item)=>item.maeData).map((item) => {
-                return item.id
-              });
+              return value.json.items.filter((item) => item.maeData).map((item) => item.id);
             }
             return [];
           }));
