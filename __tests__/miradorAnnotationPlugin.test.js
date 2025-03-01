ce3f29d132dffd06daca2513bee4c5d44cbe74bb
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-03-01 18:47:35 +0100

    Fix miradorAnnotationPlugin.test.js (including SingleCanvasDialog.js trad)

--- __tests__/miradorAnnotationPlugin.test.js
+++ __tests__/miradorAnnotationPlugin.test.js
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux';
 import { getWindowViewType } from 'mirador';
 import LocalStorageAdapter from '../src/annotationAdapter/LocalStorageAdapter';
 import MiradorAnnotation from '../src/plugins/miradorAnnotationPlugin';
-import { render, screen, fireEvent } from './test-utils';
+import { fireEvent, render, screen } from './test-utils';
 
 vi.mock('react-redux', async () => {
   const actualReactRedux = await vi.importActual('react-redux');
@@ -32,7 +32,8 @@ const defaultInitalState = {
 
 /** */
 function createWrapper(props, initalState = defaultInitalState) {
-  const mockT = vi.fn().mockImplementation((key) => key);
+  const mockT = vi.fn()
+    .mockImplementation((key) => key);
 
   return render(<MiradorAnnotation
     canvases={[]}
@@ -50,7 +51,8 @@ describe('MiradorAnnotation', () => {
   it('renders a create new button', () => {
     createWrapper();
     const button = screen.getByRole('button', { name: /create_annotation/i });
-    expect(button).toBeInTheDocument();
+    expect(button)
+      .toBeInTheDocument();
   });
 
   it('opens a new companionWindow when clicked', () => {
@@ -63,57 +65,77 @@ describe('MiradorAnnotation', () => {
     const button = screen.getByRole('button', { name: /create_annotation/i });
     fireEvent.click(button);
 
-    expect(mockDispatch).toHaveBeenCalledTimes(1);
+    expect(mockDispatch)
+      .toHaveBeenCalledTimes(1);
     const dispatchedAction = mockDispatch.mock.calls[0][0];
-    expect(dispatchedAction).toEqual(
-      expect.objectContaining({
-        payload: expect.objectContaining({
-          content: 'annotationCreation',
-          position: 'right',
+    expect(dispatchedAction)
+      .toEqual(
+        expect.objectContaining({
+          payload: expect.objectContaining({
+            content: 'annotationCreation',
+            position: 'right',
+          }),
+          type: 'mirador/ADD_COMPANION_WINDOW',
+          windowId: 'windowId',
         }),
-        type: 'mirador/ADD_COMPANION_WINDOW',
-        windowId: 'windowId',
-      }),
-    );
+      );
   });
 
   it('opens single canvas view dialog if not in single view', () => {
     getWindowViewType.mockReturnValue('book');
     createWrapper();
 
-    expect(screen.queryByText('switch_view')).toBeNull();
+    expect(screen.queryByText('switch_view'))
+      .toBeNull();
 
     const button = screen.getByRole('button', { name: /create_annotation/i });
     fireEvent.click(button);
 
-    expect(screen.queryByText('switch_view')).toBeInTheDocument();
+    console.log(screen.queryByText('switch_view'));
+
+    expect(screen.queryByText('switch_view'))
+      .toBeInTheDocument();
   });
 
   it('renders no export button if export or LocalStorageAdapter are not configured', () => {
     // eslint-disable-next-line max-len
-    const stateWithoutLocalAdapter = { config: { annotation: { adapter: () => () => {}, exportLocalStorageAnnotations: true } } };
+    const stateWithoutLocalAdapter = {
+      config: {
+        annotation: {
+          adapter: () => () => {
+          },
+          exportLocalStorageAnnotations: true
+        }
+      }
+    };
     createWrapper({}, stateWithoutLocalAdapter);
     const button = screen.queryByText(/Export local annotations for visible items/i);
-    expect(button).toBeNull();
+    expect(button)
+      .toBeNull();
 
     const annotation = {
-      adapter: () => () => {}, exportLocalStorageAnnotations: false,
+      adapter: () => () => {
+      },
+      exportLocalStorageAnnotations: false,
     };
 
     const stateWithFalsyExport = { config: annotation };
     createWrapper({}, stateWithFalsyExport);
     const button2 = screen.queryByText(/Export local annotations for visible items/i);
-    expect(button2).toBeNull();
+    expect(button2)
+      .toBeNull();
   });
 
   it('renders export button if export and LocalStorageAdapter are configured', () => {
     const annotation = {
-      adapter: () => new LocalStorageAdapter(), exportLocalStorageAnnotations: true,
+      adapter: () => new LocalStorageAdapter(),
+      exportLocalStorageAnnotations: true,
     };
 
     const initalState = { config: { annotation } };
     createWrapper({}, initalState);
     const button = screen.getByRole('button', { name: /Export local annotations for visible items/i });
-    expect(button).toBeInTheDocument();
+    expect(button)
+      .toBeInTheDocument();
   });
 });
--- src/SingleCanvasDialog.js
+++ src/SingleCanvasDialog.js
@@ -8,8 +8,6 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
 import { Tooltip } from '@mui/material';
-import { withTranslation } from 'react-i18next';
-import i18n from 'i18next';
 
 /**
  * Dialog to enforce single view for annotation creation / editing
@@ -50,24 +48,23 @@ class SingleCanvasDialog extends Component {
       >
         <DialogTitle id="single-canvas-dialog-title" disableTypography>
           <Typography variant="h2">
-            Switch view type to single view?
+            {t('switch_view_h2')}
           </Typography>
         </DialogTitle>
         <DialogContent>
           <DialogContentText variant="body1" color="inherit">
-            Annotations can only be edited in single canvas view type.
-            Switch view type to single view now?
+            {t('switch_view_content')}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
           <Tooltip title="Cancel">
             <Button onClick={handleClose}>
-              Cancel
+              {t('cancel')}
             </Button>
           </Tooltip>
           <Tooltip title="Switch to single view">
             <Button color="primary" onClick={this.confirm} variant="contained">
-              Switch to single view
+              {t('switch_view')}
             </Button>
           </Tooltip>
         </DialogActions>
@@ -80,6 +77,7 @@ SingleCanvasDialog.propTypes = {
   handleClose: PropTypes.func.isRequired,
   open: PropTypes.bool.isRequired,
   switchToSingleCanvasView: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired,
 };
 
 export default (SingleCanvasDialog);
