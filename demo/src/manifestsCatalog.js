80a69468388bdea005c01e7a15c4cb856429750d
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-03-21 15:58:17 +0100

    Fix mirador config

--- demo/src/manifestsCatalog.js
+++ demo/src/manifestsCatalog.js
@@ -18,4 +18,5 @@ export const manifestsCatalog = [
   { manifestId: 'https://preview.iiif.io/cookbook/master/recipe/0003-mvm-video/manifest.json' },
   { manifestId: 'https://iiif.io/api/cookbook/recipe/0065-opera-multiple-canvases/manifest.json' },
   { manifestId: 'https://iiif.io/api/cookbook/recipe/0064-opera-one-canvas/manifest.json' },
+  { manifestId: 'https://files.tetras-libre.fr/dev/youtube_big_media.json' },
 ];
