0ef998950670e2e98911d3c869870942fb10f6c0
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-04-12 23:42:51 +0200

    Update TextEditor.js

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
@@ -14,28 +14,27 @@ const StyledReactQuill = styled(ReactQuill)(({ theme }) => ({
 
 /** Rich text editor for annotation body */
 function TextEditor({
-  annoHtml,
-  updateAnnotationBody,
+  text,
+  setText,
 }) {
-  const [editorHtml, setEditorHtml] = useState(annoHtml);
   const annotationConfig = useSelector((state) => getConfig(state)).annotation;
-  const { formats, modules } = annotationConfig.quillConfig;
+  const {
+    formats,
+    modules,
+  } = annotationConfig.quillConfig;
   /**
    * Handle Change On ReactQuil Editor
    * @param html
    */
   const handleChange = (html) => {
-    setEditorHtml(html);
-    if (updateAnnotationBody) {
-      updateAnnotationBody(html);
-    }
+    setText(html);
   };
 
   // Data field is needed to set bounds for the editor and avoir tooltip overflow
   return (
     <div data-text-editor="name" data-testid="textEditor">
       <StyledReactQuill
-        value={editorHtml}
+        value={text}
         onChange={handleChange}
         placeholder="Your text here"
         bounds='[data-text-editor="name"]'
@@ -47,8 +46,8 @@ function TextEditor({
 }
 
 TextEditor.propTypes = {
-  annoHtml: PropTypes.string.isRequired,
-  updateAnnotationBody: PropTypes.func.isRequired,
+  setText: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default TextEditor;
