9e6650d01dd809eef4c1176d6380dade4d6f5bf1
Fabian Stoehr <stoehr@ub.uni-leipzig.de>
2025-05-08 17:16:52 +0200

    refactor: useTranslation hook to prevent props drilling

--- src/annotationForm/TargetSpatialInput.js
+++ src/annotationForm/TargetSpatialInput.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
 import { Grid } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 import AnnotationDrawing from './AnnotationFormOverlay/AnnotationDrawing';
 import { getTargetSVGToolState, TARGET_VIEW } from './AnnotationFormUtils';
 import AnnotationFormOverlay from './AnnotationFormOverlay/AnnotationFormOverlay';
@@ -11,10 +12,10 @@ import { KONVA_MODE } from './AnnotationFormOverlay/KonvaDrawing/KonvaUtils';
 export function TargetSpatialInput({
   playerReferences,
   setTargetDrawingState,
-  t,
   targetDrawingState,
   windowId,
 }) {
+  const { t } = useTranslation();
   // TODO the targetSVGToolSTate is not used. Why the defaultToolState is used?
   const [toolState, setToolState] = useState(getTargetSVGToolState());
   const [viewTool, setViewTool] = useState(TARGET_VIEW);
@@ -130,7 +131,6 @@ TargetSpatialInput.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
   playerReferences: PropTypes.object.isRequired,
   setTargetDrawingState: PropTypes.func.isRequired,
-  t: PropTypes.func.isRequired,
   // eslint-disable-next-line react/forbid-prop-types
   targetDrawingState: PropTypes.object.isRequired,
   windowId: PropTypes.string.isRequired,
