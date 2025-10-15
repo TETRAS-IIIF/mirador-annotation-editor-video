e5ed671dde9ed5081a8b9bc83a84b7409f3005f3
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-10-15 14:59:08 +0200

    Move deployment to main branch

--- vite.config.js
+++ vite.config.js
@@ -15,13 +15,13 @@ export default defineConfig({
   ...(process.env.GITHUB_PAGES
     ? {
       build: {
-        outDir: 'dist',
         emptyOutDir: true,
+        outDir: 'dist',
         rollupOptions: {
           external: ['__tests__/*', '__mocks__/*'],
-          input: fileURLToPath(new URL('./demo/src/index.html', import.meta.url)),
+          input: fileURLToPath(new URL('./demo/src/index.html', import.meta.url))
         },
-        sourcemap: true,
+        sourcemap: true
       },
     }
     : {
