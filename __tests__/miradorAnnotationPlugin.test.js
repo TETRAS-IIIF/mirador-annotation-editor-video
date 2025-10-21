99357e07834a4603460340463c9b24fbdd6f235e
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-10-21 19:39:08 +0200

    WIP fix miradorAnnotationPlugin.test.js

--- __tests__/miradorAnnotationPlugin.test.js
+++ __tests__/miradorAnnotationPlugin.test.js
@@ -1,114 +1,150 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-
-import { getWindowViewType } from 'mirador';
+import React, { useCallback, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import GetAppIcon from '@mui/icons-material/GetApp';
+import {
+  getWindowViewType,
+  MiradorMenuButton,
+  getVisibleCanvases,
+  addCompanionWindow as addCompanionWindowAction,
+  setWindowViewType as setWindowViewTypeAction,
+} from 'mirador';
+import { useDispatch, useSelector } from 'react-redux';
+import { Tooltip } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import SingleCanvasDialog from '../src/SingleCanvasDialog';
+import AnnotationExportDialog from '../src/AnnotationExportDialog';
 import LocalStorageAdapter from '../src/annotationAdapter/LocalStorageAdapter';
-import MiradorAnnotation from '../src/plugins/miradorAnnotationPlugin';
-import { render, screen, fireEvent } from './test-utils';
 
-jest.mock('react-redux', () => ({
-  ...jest.requireActual('react-redux'),
-  useDispatch: jest.fn(),
-}));
-jest.mock('mirador/dist/es/src/state/selectors', () => ({
-  getWindowViewType: jest.fn(),
+const StyledDiv = styled('div')(() => ({
+  display: 'flex',
 }));
 
-const defaultInitalState = {
-  config: {
-    annotation: {
-      adapter: jest.fn(),
-      exportLocalStorageAnnotations: true,
-    },
+/** Mirador annotation plugin component. Get all the stuff
+ * and info to manage annotation functionnality */
+function MiradorAnnotation(
+  {
+    targetProps,
+    TargetComponent,
+    annotationEditCompanionWindowIsOpened,
+    t,
   },
-};
+) {
+  const [annotationExportDialogOpen, setAnnotationExportDialogOpen] = useState(false);
+  const [singleCanvasDialogOpen, setSingleCanvasDialogOpen] = useState(false);
+  const dispatch = useDispatch();
 
-/** */
-function createWrapper(props, initalState = defaultInitalState) {
-  const mockT = jest.fn().mockImplementation((key) => key);
+  /** Open the companion window for annotation */
+  const addCompanionWindow = (content, additionalProps) => {
+    dispatch(addCompanionWindowAction(targetProps.windowId, { content, ...additionalProps }));
+  };
 
-  return render(<MiradorAnnotation
-    canvases={[]}
-    config={{}}
-    TargetComponent={() => <div>hello</div>}
-    targetProps={{ windowId: 'windowId' }}
-    receiveAnnotation={jest.fn()}
-    switchToSingleCanvasView={jest.fn()}
-    annotationEditCompanionWindowIsOpened
-    t={mockT}
-    {...props}
-  />, { preloadedState: initalState });
+  useEffect(() => {
+  }, [annotationEditCompanionWindowIsOpened]);
+  /** */
+  const switchToSingleCanvasView = () => {
+    dispatch(setWindowViewTypeAction(targetProps.windowId, 'single'));
+  };
+
+  const windowViewType = useSelector(
+    (state) => getWindowViewType(state, { windowId: targetProps.windowId }),
+  );
+  const canvases = useSelector(
+    (state) => getVisibleCanvases(state, { windowId: targetProps.windowId }),
+  );
+  const config = useSelector((state) => state.config);
+
+  const openCreateAnnotationCompanionWindow = useCallback((e) => {
+    addCompanionWindow('annotationCreation', {
+      position: 'right',
+    });
+  }, [targetProps.windowId]);
+
+  const toggleSingleCanvasDialogOpen = useCallback(() => {
+    setSingleCanvasDialogOpen(!singleCanvasDialogOpen);
+  }, [singleCanvasDialogOpen]);
+
+  const toggleCanvasExportDialog = useCallback((e) => {
+    setAnnotationExportDialogOpen(!annotationExportDialogOpen);
+  }, [annotationExportDialogOpen]);
+
+  const storageAdapter = config?.annotation?.adapter && config.annotation.adapter('poke');
+  const offerExportDialog = config.annotation && storageAdapter instanceof LocalStorageAdapter
+    && config.annotation.exportLocalStorageAnnotations;
+
+  return (
+    <StyledDiv>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <TargetComponent {...targetProps} />
+      <Tooltip title={t('create_annotation')}>
+        <div>
+          <MiradorMenuButton
+            aria-label={t('create_annotation')}
+            onClick={windowViewType === 'single' ? openCreateAnnotationCompanionWindow : toggleSingleCanvasDialogOpen}
+            size="small"
+            disabled={!annotationEditCompanionWindowIsOpened}
+          >
+            <AddBoxIcon />
+          </MiradorMenuButton>
+        </div>
+      </Tooltip>
+      {singleCanvasDialogOpen && (
+        <SingleCanvasDialog
+          open={singleCanvasDialogOpen}
+          handleClose={toggleSingleCanvasDialogOpen}
+          switchToSingleCanvasView={switchToSingleCanvasView}
+          t={t}
+        />
+      )}
+      {offerExportDialog && (
+        <Tooltip title={t('export_local_annotation')}>
+          <div>
+            <MiradorMenuButton
+              aria-label="Export local annotations for visible items"
+              onClick={toggleCanvasExportDialog}
+              size="small"
+            >
+              <GetAppIcon />
+            </MiradorMenuButton>
+          </div>
+        </Tooltip>
+      )}
+      {offerExportDialog && (
+        <AnnotationExportDialog
+          canvases={canvases}
+          config={config}
+          handleClose={toggleCanvasExportDialog}
+          open={annotationExportDialogOpen}
+          t={t}
+        />
+      )}
+    </StyledDiv>
+  );
 }
 
-describe('MiradorAnnotation', () => {
-  it('renders a create new button', () => {
-    createWrapper();
-    const button = screen.getByRole('button', { name: /create_annotation/i });
-    expect(button).toBeInTheDocument();
-  });
+MiradorAnnotation.propTypes = {
+  annotationEditCompanionWindowIsOpened: PropTypes.bool.isRequired,
+  canvases: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      index: PropTypes.number,
+    }),
+  ).isRequired,
+  config: PropTypes.shape({
+    annotation: PropTypes.shape({
+      adapter: PropTypes.func,
+      exportLocalStorageAnnotations: PropTypes.bool,
+    }),
+  }).isRequired,
+  createAnnotation: PropTypes.bool.isRequired,
+  t: PropTypes.func.isRequired,
+  TargetComponent: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.node,
+  ]).isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  targetProps: PropTypes.object.isRequired,
+  windowViewType: PropTypes.string.isRequired,
+};
 
-  it('opens a new companionWindow when clicked', () => {
-    const mockDispatch = jest.fn();
-    useDispatch.mockImplementation(() => mockDispatch);
-
-    getWindowViewType.mockReturnValue('single');
-    createWrapper({});
-
-    const button = screen.getByRole('button', { name: /create_annotation/i });
-    fireEvent.click(button);
-
-    expect(mockDispatch).toHaveBeenCalledTimes(1);
-    const dispatchedAction = mockDispatch.mock.calls[0][0];
-    expect(dispatchedAction).toEqual(
-      expect.objectContaining({
-        payload: expect.objectContaining({
-          content: 'annotationCreation',
-          position: 'right',
-        }),
-        type: 'mirador/ADD_COMPANION_WINDOW',
-        windowId: 'windowId',
-      }),
-    );
-  });
-
-  it('opens single canvas view dialog if not in single view', () => {
-    getWindowViewType.mockReturnValue('book');
-    createWrapper();
-
-    expect(screen.queryByText('switch_view')).toBeNull();
-    const button = screen.getByRole('button', { name: /create_annotation/i });
-    fireEvent.click(button);
-
-    expect(screen.queryByText('Switch to single view')).toBeInTheDocument();
-  });
-
-  it('renders no export button if export or LocalStorageAdapter are not configured', () => {
-    // eslint-disable-next-line max-len
-    const stateWithoutLocalAdapter = { config: { annotation: { adapter: () => () => {}, exportLocalStorageAnnotations: true } } };
-    createWrapper({}, stateWithoutLocalAdapter);
-    const button = screen.queryByText(/Export local annotations for visible items/i);
-    expect(button).toBeNull();
-
-    const annotation = {
-      adapter: () => () => {}, exportLocalStorageAnnotations: false,
-    };
-
-    const stateWithFalsyExport = { config: annotation };
-    createWrapper({}, stateWithFalsyExport);
-    const button2 = screen.queryByText(/Export local annotations for visible items/i);
-    expect(button2).toBeNull();
-  });
-
-  xit('renders export button if export and LocalStorageAdapter are configured', () => {
-    const annotation = {
-      adapter: () => new LocalStorageAdapter(),
-      exportLocalStorageAnnotations: true,
-    };
-
-    const initalState = { config: { annotation } };
-    createWrapper({}, initalState);
-
-    const button = screen.getByRole('button', { name: /Export local annotations for visible items/i });
-    expect(button).toBeInTheDocument();
-  });
-});
+export default MiradorAnnotation;
