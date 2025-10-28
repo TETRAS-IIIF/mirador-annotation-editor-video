b53f051e2394094dc463a38007cede5eba82d02d
Drilio <Roy-Antoine93@proton.me>
2025-10-28 12:47:21 +0100

    fixing reactQuill config error and npm bundling

--- src/TextEditor.jsx
+++ src/TextEditor.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
@@ -6,48 +6,77 @@ import { styled } from '@mui/material/styles';
 import { useSelector } from 'react-redux';
 import { getConfig } from 'mirador';
 
+const DEFAULT_QUILL_CONFIG = {
+    // https://quilljs.com/docs/formats
+    formats: [
+        'header',
+        'bold',
+        'italic',
+        'underline',
+        'strike',
+        'blockquote',
+        'list',
+        'indent',
+        'link',
+        'image',
+        'color',
+        'background',
+    ],
+    // https://quilljs.com/docs/modules/toolbar/
+    modules: {
+        toolbar: [
+            [{ header: [1, 2, false] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [
+                { list: 'ordered' },
+                { list: 'bullet' },
+                { indent: '-1' },
+                { indent: '+1' },
+            ],
+            [{ color: [] }, { background: [] }],
+            ['link', 'image'],
+            ['clean'],
+        ],
+    },
+};
+
 const StyledReactQuill = styled(ReactQuill)(({ theme }) => ({
-  '.ql-editor': {
-    minHeight: '150px',
-  },
+    '.ql-editor': {
+        minHeight: '150px',
+    },
 }));
 
 /** Rich text editor for annotation body */
-function TextEditor({
-  text,
-  setText,
-}) {
-  const annotationConfig = useSelector((state) => getConfig(state)).annotation;
-  const {
-    formats,
-    modules,
-  } = annotationConfig.quillConfig;
-  /**
-   * Handle Change On ReactQuil Editor
-   * @param html
-   */
-  const handleChange = (html) => {
-    setText(html);
-  };
+function TextEditor({ text, setText }) {
+    const quillConfigFromState = useSelector((state) => getConfig(state)?.annotation?.quillConfig);
 
-  // Data field is needed to set bounds for the editor and avoir tooltip overflow
-  return (
-    <div data-text-editor="name" data-testid="textEditor">
-      <StyledReactQuill
-        value={text}
-        onChange={handleChange}
-        placeholder="Your text here"
-        bounds='[data-text-editor="name"]'
-        modules={modules}
-        formats={formats}
-      />
-    </div>
-  );
+    const { formats, modules } = useMemo(() => {
+        return quillConfigFromState && quillConfigFromState.modules && quillConfigFromState.formats
+            ? quillConfigFromState
+            : DEFAULT_QUILL_CONFIG;
+    }, [quillConfigFromState]);
+
+    const handleChange = (html) => {
+        setText(html);
+    };
+
+    return (
+        <div data-text-editor="name" data-testid="textEditor">
+            <StyledReactQuill
+                value={text}
+                onChange={handleChange}
+                placeholder="Your text here"
+                bounds='[data-text-editor="name"]'
+                modules={modules}
+                formats={formats}
+            />
+        </div>
+    );
 }
 
 TextEditor.propTypes = {
-  setText: PropTypes.func.isRequired,
-  text: PropTypes.string.isRequired,
+    setText: PropTypes.func.isRequired,
+    text: PropTypes.string.isRequired,
 };
 
 export default TextEditor;
