b44b198d5853e79cf958159794e0568c494ff3c5
Drilio <Roy-Antoine93@proton.me>
2025-11-13 16:01:40 +0100

    FIX issue where react quill overlap parent comp

--- src/TextEditor.jsx
+++ src/TextEditor.jsx
@@ -5,45 +5,58 @@ import 'react-quill/dist/quill.snow.css';
 import { styled } from '@mui/material/styles';
 import { useSelector } from 'react-redux';
 import { getConfig } from 'mirador';
-import {DEFAULT_QUILL_CONFIG} from "./utils";
+import { DEFAULT_QUILL_CONFIG } from './utils';
 
 const StyledReactQuill = styled(ReactQuill)(({ theme }) => ({
-    '.ql-editor': {
-        minHeight: '150px',
-    },
+  '.ql-container': {
+    maxWidth: '100%',
+    width: '100%',
+  },
+  '.ql-editor': {
+    maxWidth: '100%',
+    minHeight: '150px',
+    overflowWrap: 'break-word',
+    whiteSpace: 'pre-wrap',
+    width: '100%',
+    wordBreak: 'break-word',
+  },
+  maxWidth: '100%',
+  width: '100%',
 }));
 
 /** Rich text editor for annotation body */
 function TextEditor({ text, setText }) {
-    const quillConfigFromState = useSelector((state) => getConfig(state)?.annotation?.quillConfig);
+  const quillConfigFromState = useSelector((state) => getConfig(state)?.annotation?.quillConfig);
 
-    const { formats, modules } = useMemo(() => {
-        return quillConfigFromState?.modules && quillConfigFromState?.formats
-            ? quillConfigFromState
-            : DEFAULT_QUILL_CONFIG;
-    }, [quillConfigFromState]);
+  // eslint-disable-next-line max-len
+  const { formats, modules } = useMemo(() => (quillConfigFromState?.modules && quillConfigFromState?.formats
+    ? quillConfigFromState
+    : DEFAULT_QUILL_CONFIG), [quillConfigFromState]);
+    /**
+     * Update local text state from editor HTML.
+     * @param {string} html HTML content emitted by the editor.
+     */
+  const handleChange = (html) => {
+    setText(html);
+  };
 
-    const handleChange = (html) => {
-        setText(html);
-    };
-
-    return (
-        <div data-text-editor="name" data-testid="textEditor">
-            <StyledReactQuill
-                value={text}
-                onChange={handleChange}
-                placeholder="Your text here"
-                bounds='[data-text-editor="name"]'
-                modules={modules}
-                formats={formats}
-            />
-        </div>
-    );
+  return (
+    <div data-text-editor="name" data-testid="textEditor">
+      <StyledReactQuill
+        value={text}
+        onChange={handleChange}
+        placeholder="Your text here"
+        bounds='[data-text-editor="name"]'
+        modules={modules}
+        formats={formats}
+      />
+    </div>
+  );
 }
 
 TextEditor.propTypes = {
-    setText: PropTypes.func.isRequired,
-    text: PropTypes.string.isRequired,
+  setText: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default TextEditor;
